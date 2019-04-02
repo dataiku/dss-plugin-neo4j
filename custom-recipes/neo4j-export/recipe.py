91f0d16481a4427e7cc4171dba0886fd602ec436
admin <admin>
2019-04-02 17:44:21 +0200

    Edited file 'custom-recipes/neo4j-export/recipe.py'

--- custom-recipes/neo4j-export/recipe.py
+++ custom-recipes/neo4j-export/recipe.py
@@ -123,7 +123,6 @@ try:
     r = graph.run(q)
     logger.info("[+] Loading complete")
     logger.info(r.stats())
-    logger.info("\n")
 except Exception, e:
     logger.error("[-] Issue while loading CSV")
     logger.error("[-] {}\n".format(str(e)))
