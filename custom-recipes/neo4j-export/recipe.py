b53b580b1677850e03723c8932db82f839bd6b69
admin <admin>
2019-04-02 10:18:25 +0200

    Edited file 'custom-recipes/neo4j-export/recipe.py'

--- custom-recipes/neo4j-export/recipe.py
+++ custom-recipes/neo4j-export/recipe.py
@@ -1,23 +1,6 @@
-# Code for custom code recipe neo4j-export (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
 from dataiku.customrecipe import *
 
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
 
 # To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
 input_A_names = get_input_names_for_role('input_A_role')
