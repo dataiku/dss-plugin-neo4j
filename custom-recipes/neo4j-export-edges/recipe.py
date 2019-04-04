8ca7a6fc7fa8bd4a965cd14506f791941b2c1ea9
admin <admin>
2019-04-04 11:39:34 +0200

    Edited file 'custom-recipes/neo4j-export-edges/recipe.py'

--- custom-recipes/neo4j-export-edges/recipe.py
+++ custom-recipes/neo4j-export-edges/recipe.py
@@ -16,8 +16,6 @@ INPUT_DATASET_NAME    = get_input_names_for_role('input-dataset')[0]
 OUTPUT_FOLDER_NAME    = get_output_names_for_role('output-folder')[0]
 
 # Recipe settings
-GRAPH_NODES_LABEL     = get_recipe_config().get('graph-nodes-label', None)
-GRAPH_NODES_DELETE    = get_recipe_config().get('graph-nodes-delete', False)
 NEO4J_URI             = get_recipe_config().get('neo4j-uri', None)
 NEO4J_USER            = get_recipe_config().get('neo4j-user', None)
 NEO4J_PASSWORD        = get_recipe_config().get('neo4j-password', None)
