3ea73ad6827ab63ed4319a065d0bab8c0249ec80
admin <admin>
2019-04-05 10:49:01 +0200

    Edited file 'custom-recipes/neo4j-export-relationships/recipe.py'

--- custom-recipes/neo4j-export-relationships/recipe.py
+++ custom-recipes/neo4j-export-relationships/recipe.py
@@ -56,14 +56,8 @@ logger.info("*"*80)
 # EXPORTING TO CSV
 #==============================================================================
 
-logging.info("[+] Reading dataset as dataframe...")
-ds = dataiku.Dataset(INPUT_DATASET_NAME)
-df = ds.get_dataframe()
-logger.info("[+] Read dataset with {} rows and {} columns\n".format(df.shape[0], df.shape[1]))
-
-logger.info("[+] Exporting input dataframe to CSV...")
-df.to_csv(path_or_buf=os.path.join(out_folder, 'export.csv'), sep="|",header=False, index=False)
-logger.info("[+] Exported to CSV\n")
+export_file = os.path.join(export_folder, EXPORT_FILE_NAME)
+export_dataset(dataset=INPUT_DATASET_NAME, output_file=export_file)
 
 
 #==============================================================================
