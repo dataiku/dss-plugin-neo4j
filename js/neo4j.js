de2f58ec23277a9eef18c9b224163ed4e975babb
thomas.cabrol <thomas.cabrol>
2019-05-14 22:19:01 +0200

    Edited files

--- js/neo4j.js
+++ js/neo4j.js
@@ -2,4 +2,5 @@ var app = angular.module('neo4j.module', []);
 
 app.controller('Neo4jController', function($scope) {
     // Placeholder if we want to add more advanced behavior to the plugin
+    $scope.config.neo4jUri = "bolt://localhost:7687";
 });
