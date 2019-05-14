6bad59f25860ae982d0b7c492062a22b35066fda
thomas.cabrol <thomas.cabrol>
2019-05-14 22:48:56 +0200

    Edited file 'python-connectors/neo4j-dataset/connector.py'

--- python-connectors/neo4j-dataset/connector.py
+++ python-connectors/neo4j-dataset/connector.py
@@ -25,6 +25,8 @@ class Neo4jConnector(Connector):
 
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                             partition_id=None, records_limit = -1):
+        for i in xrange(1,10):
+            yield { "first_col" : str(i), "my_string" : "Yes" }
         #if self.config["queryMode"] == "nodes":
         #    q = "MATCH (n:{}) RETURN n".format(self.config["nodeType"])
         #    r = self.graph.run(q)
