2f1871fa97cd8d9dbe6fa85e7ef753912f6bc22d
thomas.cabrol <thomas.cabrol>
2019-05-14 22:20:33 +0200

    Edited files

--- js/neo4j.js
+++ js/neo4j.js
@@ -3,4 +3,6 @@ var app = angular.module('neo4j.module', []);
 app.controller('Neo4jController', function($scope) {
     // Placeholder if we want to add more advanced behavior to the plugin
     $scope.config.neo4jUri = "bolt://localhost:7687";
+    $scope.config.neo4jUsername = "neo4j";
+    $scope.config.neo4jPassword = "dataiku";
 });
