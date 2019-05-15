526ec03ec1330c3f8b820a963cc6bdd6f93c5fc9
thomas.cabrol <thomas.cabrol>
2019-05-15 16:15:39 +0200

    Edited file 'js/neo4j.js'

--- js/neo4j.js
+++ js/neo4j.js
@@ -2,11 +2,6 @@ var app = angular.module('neo4j.module', []);
 
 app.controller('Neo4jController', function($scope) {
     
-    // To remove !!
-    $scope.config.neo4jUri = "bolt://localhost:7687";
-    $scope.config.neo4jUsername = "neo4j";
-    $scope.config.neo4jPassword = "dataiku";
-    
     // Use Python backend to get nodes list
     var updateNodes = function() {
         $scope.callPythonDo({}).then(function(data) {
