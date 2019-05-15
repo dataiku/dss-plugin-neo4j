57c24ad0c8972137b8b3f3a6eb1754d910329f95
thomas.cabrol <thomas.cabrol>
2019-05-15 16:25:41 +0200

    Added component 'neo4j-macro' (Custom runnable)

---
+++ python-runnables/neo4j-macro/runnable.py
@@ -0,0 +1,30 @@
+# This file is the actual code for the Python runnable neo4j-macro
+from dataiku.runnables import Runnable
+
+class MyRunnable(Runnable):
+    """The base interface for a Python runnable"""
+
+    def __init__(self, project_key, config, plugin_config):
+        """
+        :param project_key: the project in which the runnable executes
+        :param config: the dict of the configuration of the object
+        :param plugin_config: contains the plugin settings
+        """
+        self.project_key = project_key
+        self.config = config
+        self.plugin_config = plugin_config
+        
+    def get_progress_target(self):
+        """
+        If the runnable will return some progress info, have this function return a tuple of 
+        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
+        """
+        return None
+
+    def run(self, progress_callback):
+        """
+        Do stuff here. Can return a string or raise an exception.
+        The progress_callback is a function expecting 1 value: current progress
+        """
+        raise Exception("unimplemented")
+        
