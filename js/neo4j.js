0571665a3a291c3ed2e392de5ada16e5a86bd318
thomas.cabrol <thomas.cabrol>
2019-05-15 14:59:05 +0200

    Edited file 'js/neo4j.js'

--- js/neo4j.js
+++ js/neo4j.js
@@ -1,10 +1,13 @@
 var app = angular.module('neo4j.module', []);
 
 app.controller('Neo4jController', function($scope) {
-    // Placeholder if we want to add more advanced behavior to the plugin
-    //$scope.config.neo4jUri = "bolt://localhost:7687";
-    //$scope.config.neo4jUsername = "neo4j";
-    //$scope.config.neo4jPassword = "dataiku";
+    
+    // To remove !!
+    $scope.config.neo4jUri = "bolt://localhost:7687";
+    $scope.config.neo4jUsername = "neo4j";
+    $scope.config.neo4jPassword = "dataiku";
+    
+    // Use Python backend to get nodes list
     var updateNodes = function() {
         $scope.callPythonDo({}).then(function(data) {
             // success
@@ -15,5 +18,6 @@ app.controller('Neo4jController', function($scope) {
         });
     };
     updateNodes();
-    $scope.$watch('config.filterColumn', updateNodes);
+    $scope.$watch('config.nodeType', updateNodes);
+    
 });
