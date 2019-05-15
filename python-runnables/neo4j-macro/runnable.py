ae93e865c53990b475c96cebaa596d2c20eb8fb8
thomas.cabrol <thomas.cabrol>
2019-05-15 16:55:47 +0200

    Edited file 'python-runnables/neo4j-macro/runnable.py'

--- python-runnables/neo4j-macro/runnable.py
+++ python-runnables/neo4j-macro/runnable.py
@@ -1,17 +1,10 @@
-
+from py2neo import Graph
 from dataiku.runnables import Runnable
 
 class MyRunnable(Runnable):
 
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
-        self.project_key = project_key
         self.config = config
-        self.plugin_config = plugin_config
         
     def get_progress_target(self):
         """
