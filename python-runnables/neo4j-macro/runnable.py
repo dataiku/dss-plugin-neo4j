6ebf540476d450aff0a76d1841a7cda879521f60
thomas.cabrol <thomas.cabrol>
2019-05-15 17:10:28 +0200

    Edited file 'python-runnables/neo4j-macro/runnable.py'

--- python-runnables/neo4j-macro/runnable.py
+++ python-runnables/neo4j-macro/runnable.py
@@ -28,7 +28,9 @@ class MyRunnable(Runnable):
         #html = html + "<hr/>"
         q = self.config.get("cypherQuery")
         html = html + "<h5>Query</h5>"
+        html = html + "<pre>"
         html = html + q
+        html = html + "</pre>"
         r = self.graph.run(q)
         html = html + "<h5>Query statistics</h5>"
         html = html + "<pre>"
