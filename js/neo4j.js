fe42da17b1a737c291dda6361f93bd288a635b5d
thomas.cabrol <thomas.cabrol>
2019-05-15 14:38:02 +0200

    Edited file 'js/neo4j.js'

--- js/neo4j.js
+++ js/neo4j.js
@@ -5,4 +5,15 @@ app.controller('Neo4jController', function($scope) {
     //$scope.config.neo4jUri = "bolt://localhost:7687";
     //$scope.config.neo4jUsername = "neo4j";
     //$scope.config.neo4jPassword = "dataiku";
+    var updateNodes = function() {
+        $scope.callPythonDo({}).then(function(data) {
+            // success
+            $scope.nodes = data.nodes;
+        }, function(data) {
+            // failure
+            $scope.nodes = [];
+        });
+    };
+    updateNodes();
+    $scope.$watch('config.filterColumn', updateNodes);
 });
