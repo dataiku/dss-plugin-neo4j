b016e6421a99e6423f59cb8f48284edbeb84f538
thomas.cabrol <thomas.cabrol>
2019-05-15 15:32:03 +0200

    Edited file 'resource/neo4j.py'

--- resource/neo4j.py
+++ resource/neo4j.py
@@ -1,6 +1,7 @@
 from py2neo import Graph
 
-def do(payload, config, plugin_config, inputs):
+#def do(payload, config, plugin_config, inputs):
+def do(config, plugin_config, inputs):
     # Create Neo4j connection
     uri = config.get("neo4jUri", "bolt://localhost:7687")
     username = config.get("neo4jUsername", "neo4j")
