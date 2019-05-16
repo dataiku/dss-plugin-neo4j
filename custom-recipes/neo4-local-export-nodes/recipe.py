5b81d868feb804b8db5e8e6acadb3bbb52a431d7
thomas.cabrol <thomas.cabrol>
2019-05-16 09:24:17 +0200

    Edited file 'custom-recipes/neo4-local-export-nodes/recipe.py'

--- custom-recipes/neo4-local-export-nodes/recipe.py
+++ custom-recipes/neo4-local-export-nodes/recipe.py
@@ -62,9 +62,9 @@ export_dataset(dataset=INPUT_DATASET_NAME, output_file=export_file)
 #==============================================================================
 
 logger.info("[+] Making file available to Neo4j...")
+outfile = os.path.join(NEO4J_IMPORT_DIR, EXPORT_FILE_NAME)
 print export_file
 print outfile
-outfile = os.path.join(NEO4J_IMPORT_DIR, EXPORT_FILE_NAME)
 shutil.copyfile(export_file, outfile)
 logger.info("[+] Done.")
 
