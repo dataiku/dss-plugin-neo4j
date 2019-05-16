7e7282e44c9df9a0e29d06e3bbd9ae04656378cf
thomas.cabrol <thomas.cabrol>
2019-05-16 09:04:09 +0200

    Edited files

--- python-runnables/neo4j-macro/runnable.py
+++ python-runnables/neo4j-macro/runnable.py
@@ -6,10 +6,11 @@ class MyRunnable(Runnable):
 
     def __init__(self, project_key, config, plugin_config):
         self.config = config
+        self.plugin_config = plugin_config
         # Create Neo4j connection
-        uri = self.config.get("neo4jUri", None)
-        username = self.config.get("neo4jUsername", None)
-        password = self.config.get("neo4jPassword", None)
+        uri = self.plugin_config.get("neo4jUri", None)
+        username = self.plugin_config.get("neo4jUsername", None)
+        password = self.plugin_config.get("neo4jPassword", None)
         self.graph = Graph(uri, auth=(username, password))
         
     def get_progress_target(self):
