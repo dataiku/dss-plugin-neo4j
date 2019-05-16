e3da0428a25dc97009dfff6ec1d897d9c7ff32c2
admin <admin>
2019-05-16 10:27:15 +0200

    Edited file 'custom-recipes/neo4j-remote-export-nodes/recipe.py'

--- custom-recipes/neo4j-remote-export-nodes/recipe.py
+++ custom-recipes/neo4j-remote-export-nodes/recipe.py
@@ -13,17 +13,17 @@ from logging import FileHandler
 #==============================================================================
 
 # I/O settings
-INPUT_DATASET_NAME    = get_input_names_for_role('input-dataset')[0]
-OUTPUT_FOLDER_NAME    = get_output_names_for_role('output-folder')[0]
+INPUT_DATASET_NAME    = get_input_names_for_role('inputDataset')[0]
+OUTPUT_FOLDER_NAME    = get_output_names_for_role('outputFolder')[0]
 
 # Recipe settings
-GRAPH_NODES_LABEL     = get_recipe_config().get('graph-nodes-label', None)
-GRAPH_NODES_DELETE    = get_recipe_config().get('graph-nodes-delete', False)
-NEO4J_URI             = get_recipe_config().get('neo4j-uri', None)
-NEO4J_USER            = get_recipe_config().get('neo4j-user', None)
-NEO4J_PASSWORD        = get_recipe_config().get('neo4j-password', None)
-SSH_HOST              = get_recipe_config().get('ssh-host', None)
-SSH_USER              = get_recipe_config().get('ssh-user', None)
+GRAPH_NODES_LABEL     = get_recipe_config().get('graphNodesLabel', None)
+GRAPH_NODES_DELETE    = get_recipe_config().get('graphNodesDelete', False)
+NEO4J_URI             = get_recipe_config().get('neo4jUri', None)
+NEO4J_USER            = get_recipe_config().get('neo4jUsername', None)
+NEO4J_PASSWORD        = get_recipe_config().get('neo4jPassword', None)
+SSH_HOST              = get_recipe_config().get('sshHost', None)
+SSH_USER              = get_recipe_config().get('sshUsername', None)
 SSH_IMPORT_DIRECTORY  = get_recipe_config().get('ssh-import-directory', None)
 
 EXPORT_FILE_NAME      = 'export.csv'
