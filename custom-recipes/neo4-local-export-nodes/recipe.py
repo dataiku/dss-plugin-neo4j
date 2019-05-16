d3b98b4471a3781811d9a10b515dd0866b77b0ce
thomas.cabrol <thomas.cabrol>
2019-05-16 09:11:32 +0200

    Edited file 'custom-recipes/neo4-local-export-nodes/recipe.py'

--- custom-recipes/neo4-local-export-nodes/recipe.py
+++ custom-recipes/neo4-local-export-nodes/recipe.py
@@ -13,16 +13,13 @@ from logging import FileHandler
 #==============================================================================
 
 # I/O settings
-INPUT_DATASET_NAME    = get_input_names_for_role('input-dataset')[0]
-OUTPUT_FOLDER_NAME    = get_output_names_for_role('output-folder')[0]
+INPUT_DATASET_NAME    = get_input_names_for_role('inputDataset')[0]
+OUTPUT_FOLDER_NAME    = get_output_names_for_role('outputDolder')[0]
 
 # Recipe settings
-GRAPH_NODES_LABEL     = get_recipe_config().get('graph-nodes-label', None)
-GRAPH_NODES_DELETE    = get_recipe_config().get('graph-nodes-delete', False)
-NEO4J_URI             = get_recipe_config().get('neo4j-uri', None)
-NEO4J_USER            = get_recipe_config().get('neo4j-user', None)
-NEO4J_PASSWORD        = get_recipe_config().get('neo4j-password', None)
-NEO4J_IMPORT_DIR      = get_recipe_config().get('neo4j-import-dir', None)
+GRAPH_NODES_LABEL  = get_recipe_config().get('graphNodesLabel', None)
+GRAPH_NODES_DELETE = get_recipe_config().get('graphNodesDelete', False)
+NEO4J_IMPORT_DIR   = get_recipe_config().get('neo4jImportDir', None)
 
 EXPORT_FILE_NAME      = 'export.csv'
 
