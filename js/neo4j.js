19463df0ae88c04c149bdb50c80ba34018af6b5e
Joachim <joachim.zentici@dataiku.com>
2019-05-31 15:06:53 +0200

    cleanup

--- js/neo4j.js
+++ js/neo4j.js
@@ -1,19 +1,13 @@
-var app = angular.module('neo4j.module', []);
+const app = angular.module('neo4j.module', []);
 
 app.controller('Neo4jController', function($scope) {
-
-    // Use Python backend to get nodes list
-    var updateNodes = function() {
+    function updateNodes() {
         $scope.callPythonDo({}).then(function(data) {
-            // success
-            console.warn('data', data)
             $scope.nodes = data.nodes;
         }, function(data) {
-            // failure
             $scope.nodes = [];
         });
     };
     updateNodes();
     $scope.$watch('config.nodeType', updateNodes);
-
 });
--- resource/neo4j.py
+++ resource/neo4j.py
@@ -1,13 +1,12 @@
 from py2neo import Graph
 
 def do(payload, config, plugin_config, inputs):
-    # Create Neo4j connection
-    uri = plugin_config.get("neo4jUri", None)
+    uri = plugin_config.get("neo4jUri")
+    if uri is None or uri == "":
+        raise ValueError("Plugin setting: URI not specified")
     username = plugin_config.get("neo4jUsername", None)
     password = plugin_config.get("neo4jPassword", None)
     graph = Graph(uri, auth=(username, password))
-    # Get distinct nodes
-    q = """MATCH (n) RETURN DISTINCT LABELS(n) AS Labels"""
-    r = graph.run(q)
+    r = graph.run("MATCH (n) RETURN DISTINCT LABELS(n) AS Labels")
     nodes = [n["Labels"][0] for n in r.data()]
     return {'nodes': nodes}
