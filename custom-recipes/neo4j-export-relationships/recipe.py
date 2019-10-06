26fe25575b87b4b54e634de41b4d13e1258d86fe
kgued <kgued@local>
2019-10-06 22:17:23 +0200

    cleanup before merge

--- custom-recipes/neo4j-export-relationships/recipe.py
+++ custom-recipes/neo4j-export-relationships/recipe.py
@@ -22,19 +22,16 @@ params = RelationshipsExportParams(
 logger = setup_logging(output_folder)
 input_dataset_schema = input_dataset.read_schema()
 params.check(input_dataset_schema)
-export_file_name = get_export_file_name()
+export_file_fullname = os.path.join(get_export_file_path_in_folder(), get_export_file_name())
 
 # --- Run
 
-#export_file = os.path.join(output_folder, export_file_name)
-#export_dataset(input_dataset, export_file)
 export_dataset(input_dataset, output_folder)
-#neo4jhandle.move_to_import_dir(export_file)
 
 neo4jhandle.add_unique_constraint_on_relationship_nodes(params)
 if params.clear_before_run:
     neo4jhandle.delete_relationships(params)
-neo4jhandle.load_relationships(export_file_name, input_dataset_schema, params)
+neo4jhandle.load_relationships(export_file_fullname, input_dataset_schema, params)
 
 # --- Cleanup
 #neo4jhandle.delete_file_from_import_dir(export_file_name)
--- python-lib/commons.py
+++ python-lib/commons.py
@@ -15,26 +15,13 @@ logger = logging.getLogger()
 
 def setup_logging(output_folder):
     logger.setLevel(logging.INFO)
-    # FIXME - do we really need this ?
-    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-
-    # FIXME - do we really need this ?
-    #export_log = os.path.join(output_folder, 'export.log')
-    #file_handler = logging.FileHandler(export_log, 'w')
-    #file_handler.setFormatter(formatter)
-    #logger.addHandler(file_handler)
     return logger
 
 def get_neo4jhandle():
     neo4jhandle = Neo4jHandle(
         get_plugin_config().get('neo4jUri'),
         get_plugin_config().get('neo4jUsername'),
-        get_plugin_config().get('neo4jPassword'),
-        #TODO remove all of those when clean
-        get_plugin_config().get('neo4jImportDir'),
-        get_plugin_config().get('neo4jIsRemote', False),
-        get_plugin_config().get('sshHost', None),
-        get_plugin_config().get('sshUsername', None)
+        get_plugin_config().get('neo4jPassword')
         )
     #TODO clean this - can be useless
     neo4jhandle.check()
@@ -52,30 +39,14 @@ def get_input_output():
     output_folder = dataiku.Folder(output_folder_name)
     return (input_dataset, output_folder)
 
-#TODO clean this
-#def get_export_file_name():
-#    output_folder_name = get_output_names_for_role('output_folder')[0]
-#    return 'dss_neo4j_export_' + output_folder_name + '.csv'
-
-# TODO clean this
-#def export_dataset(dataset, output_file, format="tsv-excel-noheader"):
-#    """
-#    exports a Dataiku Dataset to CSV with no need to go through a Pandas dataframe first (string only)
-#    """
-#    logger.info("[+] Starting file export...")
-#    with open(output_file, "w") as o:
-#        with dataset.raw_formatted_data(format=format) as i:
-#            while True:
-#                chunk = i.read(32000)
-#                if len(chunk) == 0:
-#                    break
-#                o.write(chunk)
-#    logger.info("[+] Export done.")
-
 def get_export_file_name():
     (input_dataset, output_folder) = get_input_output()
     # project hierarchy already exist in the folder
     return 'dss_neo4j_export_{}.csv'.format(input_dataset.short_name)
+
+def get_export_file_path_in_folder():
+    (input_dataset, output_folder) = get_input_output()
+    return os.path.join(output_folder.project_key, output_folder.short_name)
 
 def export_dataset(dataset, output_folder, format="tsv-excel-noheader"):
     if not isinstance(output_folder, dataiku.Folder):
