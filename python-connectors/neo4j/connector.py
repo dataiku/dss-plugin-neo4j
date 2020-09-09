63c9a6e06f0a273faa9167e2ea0d92dc0ffe700e
StanislasGuinel <stanislas.guinel@dataiku.com>
2020-09-09 18:40:59 +0200

    improve dataset component and update py2neo version

--- python-connectors/neo4j/connector.py
+++ python-connectors/neo4j/connector.py
@@ -10,7 +10,7 @@ class MyConnector(Connector):
         self.config = config
         self.plugin_config = plugin_config
 
-        settings = config.get('neo4j_server_configuration')
+        settings = config.get("neo4j_server_configuration")
         try:
             self.graph = Graph(settings.get("neo4j_uri"), auth=(settings.get("neo4j_username"), settings.get("neo4j_password")))
         except Exception:
@@ -23,11 +23,15 @@ class MyConnector(Connector):
         if self.config["select_nodes_or_relationships"] == "select_nodes":
 
             if self.config.get("selected_node", None):
+                self._check_query_input(self.config["selected_node"], "node label")
                 query = "MATCH (n:{}) RETURN n".format(self.config["selected_node"])
                 data = self.graph.run(query)
                 for row in data:
-                    node_properties = {"neo4j_id": row['n'].identity}
-                    node_properties.update(dict(row['n']))
+                    node_properties = {
+                        "node_label": self.config["selected_node"],
+                        "neo4j_id": row["n"].identity
+                    }
+                    node_properties.update(dict(row["n"]))
                     self._convert_neotime_properties(node_properties)
                     yield node_properties
             else:
@@ -39,27 +43,33 @@ class MyConnector(Connector):
         elif self.config["select_nodes_or_relationships"] == "select_relationships":
 
             if self.config.get("selected_relationship", None):
+                self._check_query_input(self.config["selected_relationship"], "relationship type")
                 query = "MATCH ()-[r:{}]->() RETURN r".format(self.config["selected_relationship"])
                 data = self.graph.run(query)
                 for row in data:
                     rel_properties = {
-                        "source_neo4j_id": row['r'].start_node.identity,
-                        "target_neo4j_id": row['r'].end_node.identity
+                        "relationship_type": self.config["selected_relationship"],
+                        "source_neo4j_id": row["r"].start_node.identity,
+                        "target_neo4j_id": row["r"].end_node.identity
                     }
-                    rel_properties.update(dict(row['r']))
+                    rel_properties.update(dict(row["r"]))
                     self._convert_neotime_properties(rel_properties)
                     yield rel_properties
             else:
-                query = "CALL db.schema()"
-                relationships = self.graph.run(query).data()[0]['relationships']
+                query = "CALL db.schema.visualization()"
+                relationships = self.graph.run(query).data()[0]["relationships"]
                 for rel in relationships:
                     yield {
-                        "source node": rel.start_node['name'],
+                        "source node": rel.start_node["name"],
                         "relationship": type(rel).__name__,
-                        "target node": rel.end_node['name']
+                        "target node": rel.end_node["name"]
                     }
 
     def _convert_neotime_properties(self, properties):
         for k, v in properties.items():
             if isinstance(v, neotime.DateTime):
                 properties[k] = v.to_native()
+
+    def _check_query_input(self, input_str, label):
+        if "|" in input_str:
+            raise ValueError("You must enter only one valid {} !".format(label))
