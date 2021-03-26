0294c6b89b18706bd891db12c7b43542067cb12b
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-03-26 18:31:41 +0100

    [CH59793] Custom query parameter in Dataset

--- python-connectors/neo4j/connector.py
+++ python-connectors/neo4j/connector.py
@@ -1,10 +1,10 @@
 from dataiku.connector import Connector
+import logging
 from py2neo import Graph
 import neotime
 
 
 class MyConnector(Connector):
-
     def __init__(self, config, plugin_config):
         Connector.__init__(self, config, plugin_config)
         self.config = config
@@ -12,9 +12,11 @@ class MyConnector(Connector):
 
         settings = config.get("neo4j_server_configuration")
         try:
-            self.graph = Graph(settings.get("neo4j_uri"), auth=(settings.get("neo4j_username"), settings.get("neo4j_password")))
+            self.graph = Graph(
+                settings.get("neo4j_uri"), auth=(settings.get("neo4j_username"), settings.get("neo4j_password"))
+            )
         except Exception:
-            raise ValueError("Fail to connect to neo4j server !")
+            raise Exception(f"Failed to connect to the Neo4j server. Please check your preset credentials and URI.")
 
     def get_read_schema(self):
         return None
@@ -29,10 +31,7 @@ class MyConnector(Connector):
                     query += " LIMIT {}".format(records_limit)
                 data = self.graph.run(query)
                 for row in data:
-                    node_properties = {
-                        "node_label": self.config["selected_node"],
-                        "neo4j_id": row["n"].identity
-                    }
+                    node_properties = {"node_label": self.config["selected_node"], "neo4j_id": row["n"].identity}
                     node_properties.update(dict(row["n"]))
                     self._convert_neotime_properties(node_properties)
                     yield node_properties
@@ -54,7 +53,7 @@ class MyConnector(Connector):
                     rel_properties = {
                         "relationship_type": self.config["selected_relationship"],
                         "source_neo4j_id": row["r"].start_node.identity,
-                        "target_neo4j_id": row["r"].end_node.identity
+                        "target_neo4j_id": row["r"].end_node.identity,
                     }
                     rel_properties.update(dict(row["r"]))
                     self._convert_neotime_properties(rel_properties)
@@ -66,8 +65,17 @@ class MyConnector(Connector):
                     yield {
                         "source node": rel.start_node["name"],
                         "relationship": type(rel).__name__,
-                        "target node": rel.end_node["name"]
+                        "target node": rel.end_node["name"],
                     }
+
+        elif self.config["select_nodes_or_relationships"] == "custom_query":
+            custom_query = self.config.get("selected_custom_query", None)
+            logging.info(f"Neo4j plugin - Dataset - User custom query: {custom_query}")
+            if records_limit > -1:
+                custom_query = f"CALL {{{custom_query}}} RETURN * LIMIT {records_limit}"
+            results = self.graph.run(custom_query)
+            for record in results.data():
+                yield dict(record)
 
     def _convert_neotime_properties(self, properties):
         for k, v in properties.items():
