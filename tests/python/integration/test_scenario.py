ac3e3f44abc7aee51aa92afcb55a47a1f0627e02
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-04-08 16:10:10 +0200

    fix test_scenario

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -9,5 +9,5 @@ pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
 test_kwargs = {"user": "data_scientist_1", "project_key": "TESTNEO4JPLUGIN"}
 
 
-def test_run_neo4j(dss_user_clients):
-    dss_scenario.run(dss_user_clients, scenario_id="all_test", **test_kwargs)
+def test_run_neo4j(user_dss_clients):
+    dss_scenario.run(user_dss_clients, scenario_id="all_test", **test_kwargs)
