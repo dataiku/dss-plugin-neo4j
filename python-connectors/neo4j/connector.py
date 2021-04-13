aed42462a4328514633b5a7dc77eaaadaa20daf6
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-04-13 10:44:39 +0200

    improvements variables and errors

--- python-connectors/neo4j/connector.py
+++ python-connectors/neo4j/connector.py
@@ -1,3 +1,4 @@
+from neo4j.graph import Node
 from dataiku.connector import Connector
 import neo4j
 from dku_neo4j.neo4j_handle import Neo4jHandle
@@ -5,6 +6,9 @@ import logging
 
 
 class MyConnector(Connector):
+    NODE = "node"
+    RELATIONSHIP = "relationship"
+
     def __init__(self, config, plugin_config):
         Connector.__init__(self, config, plugin_config)
         self.config = config
@@ -23,9 +27,9 @@ class MyConnector(Connector):
                 if self.config["select_nodes_or_relationships"] == "select_nodes":
                     if self.config.get("selected_node", None):
                         self._check_query_input(self.config["selected_node"], "node label")
-                        query = "MATCH (n:{}) RETURN n".format(self.config["selected_node"])
+                        query = f"MATCH ({self.NODE}:{self.config['selected_node']}) RETURN {self.NODE}"
                         if records_limit > -1:
-                            query += " LIMIT {}".format(records_limit)
+                            query += f" LIMIT {records_limit}"
                         for row in session.run(query):
                             yield self._process_node(row)
                     else:
@@ -37,9 +41,9 @@ class MyConnector(Connector):
 
                     if self.config.get("selected_relationship", None):
                         self._check_query_input(self.config["selected_relationship"], "relationship type")
-                        query = "MATCH ()-[r:{}]->() RETURN r".format(self.config["selected_relationship"])
+                        query = f"MATCH ()-[{self.RELATIONSHIP}:{self.config['selected_relationship']}]->() RETURN {self.RELATIONSHIP}"
                         if records_limit > -1:
-                            query += " LIMIT {}".format(records_limit)
+                            query += f" LIMIT {records_limit}"
                         for row in session.run(query):
                             yield self._process_relationship(row)
                     else:
@@ -54,7 +58,9 @@ class MyConnector(Connector):
                                 }
 
                 elif self.config["select_nodes_or_relationships"] == "custom_query":
-                    custom_query = self.config.get("selected_custom_query", None)
+                    custom_query = self.config.get("selected_custom_query")
+                    if custom_query is None:
+                        raise Exception("Custom Cypher query is empty. Please enter a valid query.")
                     logging.info(f"Neo4j plugin - Dataset - User custom query: {custom_query}")
                     if records_limit > -1:
                         custom_query = f"CALL {{{custom_query}}} RETURN * LIMIT {records_limit}"
@@ -65,30 +71,32 @@ class MyConnector(Connector):
                         logging.error(
                             f"Neo4j plugin - Dataset - Error while running custom cypher query: {custom_query}. Error: {neo4j_error}"
                         )
-                        raise Exception(f"Custom cypher query failed. Check the logs for more details about the error.")
+                        raise Exception(
+                            f"Custom cypher query failed with error: '{neo4j_error.message}'. Check the logs for more details about the error."
+                        )
 
     def _convert_neotime_properties(self, properties):
-        for k, v in properties.items():
-            if isinstance(v, neo4j.time.DateTime):
-                properties[k] = v.to_native()
+        for key, value in properties.items():
+            if isinstance(value, neo4j.time.DateTime):
+                properties[key] = value.to_native()
 
     def _check_query_input(self, input_str, label):
         if "|" in input_str:
             raise ValueError("You must enter only one valid {} !".format(label))
 
     def _process_node(self, row):
-        node_properties = {"node_label": self.config["selected_node"], "neo4j_id": row["n"].id}
-        node_properties.update(dict(row["n"]))
+        node_properties = {"node_label": self.config["selected_node"], "neo4j_id": row[self.NODE].id}
+        node_properties.update(dict(row[self.NODE]))
         self._convert_neotime_properties(node_properties)
         return node_properties
 
     def _process_relationship(self, row):
         rel_properties = {
             "relationship_type": self.config["selected_relationship"],
-            "source_neo4j_id": row["r"].start_node.id,
-            "target_neo4j_id": row["r"].end_node.id,
+            "source_neo4j_id": row[self.RELATIONSHIP].start_node.id,
+            "target_neo4j_id": row[self.RELATIONSHIP].end_node.id,
         }
-        rel_properties.update(dict(row["r"]))
+        rel_properties.update(dict(row[self.RELATIONSHIP]))
         self._convert_neotime_properties(rel_properties)
         return rel_properties
 
@@ -96,16 +104,16 @@ class MyConnector(Connector):
         if not isinstance(record, neo4j.data.Record):
             raise ValueError("Query return type cannot be converted into a dataset. It must be a record.")
         processed_record = {}
-        for k, v in record.items():
-            if isinstance(v, (neo4j.graph.Node, neo4j.graph.Relationship)):
-                processed_record[k] = dict(v)
-                self._convert_neotime_properties(processed_record[k])
-            elif isinstance(v, neo4j.time.DateTime):
-                processed_record[k] = v.to_native()
-            elif isinstance(v, (int, float, bool, str)):
-                processed_record[k] = v
+        for key, value in record.items():
+            if isinstance(value, (neo4j.graph.Node, neo4j.graph.Relationship)):
+                processed_record[key] = dict(value)
+                self._convert_neotime_properties(processed_record[key])
+            elif isinstance(value, neo4j.time.DateTime):
+                processed_record[key] = value.to_native()
+            elif isinstance(value, (int, float, bool, str)):
+                processed_record[key] = value
             else:
-                logging.info(f"Neo4j plugin - Dataset - Conversion for return type '{type(v)}' is not implemented.")
-                processed_record[k] = str(v)
+                logging.info(f"Neo4j plugin - Dataset - Conversion for return type '{type(value)}' is not implemented.")
+                processed_record[key] = str(value)
 
         return processed_record
