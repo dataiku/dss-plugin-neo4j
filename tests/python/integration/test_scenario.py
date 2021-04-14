9b3e3f9113afd0ce9718764306822244f65b1b1e
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-04-14 14:41:35 +0200

    change integration scenario

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -4,5 +4,5 @@ from dku_plugin_test_utils import dss_scenario
 TEST_PROJECT_KEY = "TESTNEO4JPLUGIN"
 
 
-def test_run_neo4j(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="te")
+def test_run_neo4j_export(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="export")
