a826f96d03ebb0e9af21e14de0fd62d5107fad22
StanislasGuinel <stanislas.guinel@dataiku.com>
2021-04-14 14:10:33 +0200

    split integration tests

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -4,5 +4,9 @@ from dku_plugin_test_utils import dss_scenario
 TEST_PROJECT_KEY = "TESTNEO4JPLUGIN"
 
 
-def test_run_neo4j(user_dss_clients):
-    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="te")
+def test_run_neo4j_export(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="export")
+
+
+def test_run_neo4j_dataset(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id="dataset")
